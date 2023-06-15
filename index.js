f79de8c332127d967acf7134bafe742ff558321d
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2023-06-16 02:37:33 +0530

    modify production route

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT;
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
   app.use(express.static(path.resolve(__dirname, "client", "build")));
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(
       path.resolve(__dirname, "client", "build", "index.html"),
       function (err) {
